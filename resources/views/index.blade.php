5a24a77106d04faa9f14d32ec455d62984a6ee5e
HOSSAIN MD SAGAR <mdsagarhossain@gmail.com>
2022-02-22 07:44:59 +0900

    layout-view & bootstrap
    added layout view and bootstrap css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,20 @@
 
+@extends('layouts.app')
+
+@section('content')
 
 <h2>Todos</h2>
-@if (count($todos)>0)
-    @foreach ($todos as $todo)
-        <h3>{{ $todo->title }}</h3>
-        <h4>{{ $todo->description }}</h4>
-        <h4>{{ $todo->due }}</h4>
-    @endforeach
+
+    @if (count($todos)>0)
     
-@endif
+        @foreach ($todos as $todo)
+        <div class="card m-3">
+            <h3>{{ $todo->title }}</h3>
+            <h4>{{ $todo->description }}</h4>
+            <h4>{{ $todo->due }}</h4>
+         </div>
+        @endforeach
+   
+    @endif
+
+@endsection
